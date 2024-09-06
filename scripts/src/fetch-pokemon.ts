de67e1b1e4d84641b0482d122de05528da6794f2
Oasiris <11142550+Oasiris@users.noreply.github.com>
2024-09-06 15:40:27 -0700

    fetch 1 pokemon

--- scripts/src/fetch-pokemon.ts
+++ scripts/src/fetch-pokemon.ts
@@ -1,1 +1,34 @@
-console.log('hello world')
+import path from 'node:path'
+import fs from 'node:fs'
+import { awaitPrompt } from './util'
+
+const OUT_DIRECTORY = path.resolve(__dirname, '../out/pokemon-entries')
+const POKEAPI_URL = 'https://pokeapi.co/api/v2'
+
+async function main(): Promise<void> {
+    // Figure out pokemon numbers to fetch
+    const pokedexNums = []
+    for (let i = 1; i <= 251; i++) {
+        pokedexNums.push(i)
+    }
+    console.log(pokedexNums.join(', '))
+    console.log('Total entries:', pokedexNums.length)
+    await awaitPrompt('Press Enter to fetch the first pokemon entry, ' + pokedexNums[0] + ':')
+
+    // Skip the first pokemon entry if it was already saved
+    const id = pokedexNums[0]
+    const filename = path.join(OUT_DIRECTORY, `${id}.json`)
+    if (fs.existsSync(filename)) {
+        console.log(`File ${filename} already exists. Skipping fetch.`)
+        return
+    }
+    const idURL = POKEAPI_URL + `/pokemon/${id}`
+    console.log(`Fetching ${idURL}...`)
+    const response = await fetch(idURL)
+    const data = await response.json()
+    console.log(data)
+    console.log('Saving to', filename)
+    fs.writeFileSync(filename, JSON.stringify(data, null, 2))
+}
+
+main()
---
+++ scripts/src/util.ts
@@ -0,0 +1,14 @@
+import readline from 'node:readline'
+
+/**
+ * @example awaitPrompt('What is your name?') => Promise<"my user input here">
+ */
+export function awaitPrompt(query: string): Promise<string> {
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+    return new Promise((resolve) =>
+        rl.question(query, (answer) => {
+            rl.close()
+            resolve(answer)
+        }),
+    )
+}
